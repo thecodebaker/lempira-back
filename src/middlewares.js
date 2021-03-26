c2befd51fea208878e29617501b7bb2aae5ff129
David Mendoza <d.mendoza99@unitec.edu>
2021-03-25 18:19:37 -0600

    middleware para jwt

--- src/middlewares.js
+++ src/middlewares.js
@@ -14,4 +14,26 @@ const NotFound = (_req, res, _next) => {
   });
 };
 
-module.exports = { defaultError, NotFound };
+const jwt = require('jsonwebtoken');
+const user = require('./models/user.model');
+
+const secret = process.env.SECRET;
+const checkJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    // eslint-disable-next-line consistent-return
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      user.findOne({ email: decoded.email }).then((userDoc) => {
+        req.user = userDoc;
+        next();
+      });
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+module.exports = { defaultError, NotFound, checkJWT };
